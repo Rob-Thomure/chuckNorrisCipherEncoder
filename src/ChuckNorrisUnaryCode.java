d77219adcfd46e91f5ac58cce1e44e1047bd408d
Rob Thomure <robthomure@outlook.com>
2022-11-12 15:55:51 -0600

    stage 5: The program asks the user for the desired option (encode/decode/exit), performs it, and all that in a loop until the user wants to finish. Also checks for invalid inputs

--- src/ChuckNorrisUnaryCode.java
+++ src/ChuckNorrisUnaryCode.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChuckNorrisUnaryCode {
     private final String asciiBinaryString;
     private int index;
@@ -13,13 +16,80 @@ public class ChuckNorrisUnaryCode {
     public String parseChuckNorrisUnaryCode() {
         StringBuilder stringBuilder = new StringBuilder();
         this.index = 0;
-
         while (index < asciiBinaryString.length()) {
             stringBuilder.append(addFirstBlock());
             stringBuilder.append(addSecondBlock());
         }
         return stringBuilder.toString().trim();
     }
+
+    /**
+     *
+     * @param encodedMessage - a ChuckNorrisUnary coded message.
+     * @return - checks the ChuckNorrisUnary code message and returns true
+     * if includes valid characters, the first block of each sequence is 0 or 00,
+     * the number of bocks is even, and the length of the binary bits is multiple 7
+     */
+    public static boolean isValid(String encodedMessage)  {
+        String[] zeroBlocks = encodedMessage.split(" ");
+        return isValidCharacters(encodedMessage) &&
+                isValidFirstBlockSequences(zeroBlocks) &&
+                isEvenNumberZeroBlocks(zeroBlocks) &&
+                isSevenBitMultipleBits(zeroBlocks);
+    }
+
+    /**
+     * Check whether message includes characters other than 0 or spaces.
+     * @param encodedMessage - a ChuckNorrisUnary coded message.
+     * @return - returns true if message doesn't include characters other than 0 or spaces
+     */
+    private static boolean isValidCharacters(String encodedMessage) {
+        Pattern pattern = Pattern.compile("[^0+\s]");
+        Matcher matcher = pattern.matcher(encodedMessage);
+        return !matcher.find();
+    }
+
+    /**
+     * Check if the first block of each sequence is not 0 or 00.
+     * @param zeroBlocks - a String array block of 0 bits from ChuckNorris coded message.
+     * @return - returns true if the first block in each pair in only includes 0 or 00
+     */
+    private static boolean isValidFirstBlockSequences(String[] zeroBlocks){
+        for (int i = 1; i < zeroBlocks.length; i += 2) {
+            if (!zeroBlocks[i - 1].matches("0{1,2}")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if ChuckNorrisUnary coded message includes even number of 0 bit blocks
+     * @param zeroBlocks - a String array block of 0 bits from ChuckNorris coded message.
+     * @return - returns true if zeroBlocks array size is even.
+     */
+    private static boolean isEvenNumberZeroBlocks(String[] zeroBlocks) {
+        return zeroBlocks.length % 2 == 0;
+    }
+
+    /**
+     * check if the length of the decoded binary string is multiple of 7.
+     * @param zeroBlocks - a String array block of 0 bits from ChuckNorris coded message.
+     * @return -
+     */
+    private static boolean isSevenBitMultipleBits(String[] zeroBlocks) {
+        int bitCount = 0;
+        for (int i = 1; i < zeroBlocks.length; i += 2) {
+            bitCount += zeroBlocks[i].length();
+        }
+        return bitCount % 7 == 0;
+    }
+
+
+
+
+
+
 
     private String addFirstBlock() {
         leadingBitValue = asciiBinaryString.charAt(index);
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
 
-// stage 3
-// if series of 1's then 1st of dual blocks = 0
-// if series of 0's then 1st of dual blocks = 00
-// second of dual blocks number of contiguous 0's is equal to number of contiguous similar digits in ascii input
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input encoded string:");
-        String input = scanner.nextLine();
-        System.out.println("\nThe result:");
-        String binaryString = ChuckNorrisUnaryCode.toBinaryString(input);
-        String decipheredString = AsciiString.toDecryptedString(binaryString);
-
-        System.out.println(decipheredString);
+        boolean exit = false;
+        while (!exit) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please input operation (encode/decode/exit):");
+            String operation = scanner.nextLine().toLowerCase();
+            switch (operation) {
+                case "encode":
+                    System.out.println("Input string");
+                    ChuckNorrisUnaryCode chuckNorrisUnaryCode = new ChuckNorrisUnaryCode(scanner.nextLine());
+                    System.out.println("Encoded string:");
+                    System.out.println(chuckNorrisUnaryCode.parseChuckNorrisUnaryCode());
+                    System.out.println();
+                    break;
+                case "decode":
+                    System.out.println("Input encoded string:");
+                    String encodedMessage = scanner.nextLine();
+                    if (ChuckNorrisUnaryCode.isValid(encodedMessage)) {
+                        System.out.println("Decoded string:");
+                        String binaryString = ChuckNorrisUnaryCode.toBinaryString(encodedMessage);
+                        System.out.println(AsciiString.toDecryptedString(binaryString));
+                    } else {
+                        System.out.println("Encoded string is not valid.");
+                    }
+                    System.out.println();
+                    break;
+                case "exit":
+                    exit = true;
+                    System.out.println("Bye!");
+                    break;
+                default:
+                    System.out.printf("There is no '%s' operation\n\n", operation);
+                    break;
+            }
+        }
     }
-
 }
